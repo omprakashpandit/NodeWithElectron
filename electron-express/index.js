226a70b7d336188bccccc2205505cd95354c47df
omprakashpandit <32616762+omprakashpandit@users.noreply.github.com>
2024-08-08 11:07:24 +0530

    Add files via upload

---
+++ electron-express/index.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+ // res.send('Hello World!')
+  res.sendFile('index.html', {root: __dirname })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
